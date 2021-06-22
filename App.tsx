69e68a9731b07d8c07602c7d78b90e42d3577cb8
Gui-dev <adrianon10@gmail.com>
2021-06-22 03:47:01 -0300

    refactor: remove component Background

--- App.tsx
+++ App.tsx
@@ -2,14 +2,13 @@ import 'react-native-gesture-handler'
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 
-import { Background } from './src/components/Background'
 import { Routes } from './src/routes'
 
 export default function App() {
   return (
-    <Background>
+    <>
       <StatusBar style="light" />
       <Routes/>
-    </Background>
+    </>
   );
 }
