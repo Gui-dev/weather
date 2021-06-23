a84a12e24adde75d22c484a15481365eb3d8da6a
Gui-dev <adrianon10@gmail.com>
2021-06-23 02:16:44 -0300

    feat: add component Load

---
+++ src/components/Load/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import LottieView from 'lottie-react-native'
+
+import weatherLoad from './../../assets/weather-load.json'
+import { Container } from './style'
+
+const Load: React.FC = () => {
+  return (
+    <Container>
+      <LottieView
+        source={ weatherLoad }
+        autoPlay
+        loop
+        style={{
+          width: 200,
+          height: 200,
+          backgroundColor: 'transparent'
+        }}
+      />
+    </Container>
+  );
+}
+
+export default Load;
