0db2d0d012085a2a2412d4fd91734b5de3a6b385
Gui-dev <adrianon10@gmail.com>
2021-06-23 02:17:24 -0300

    feat: add component Load and google font montserrat

--- App.tsx
+++ App.tsx
@@ -1,14 +1,25 @@
 import 'react-native-gesture-handler'
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { useFonts, Montserrat_400Regular, Montserrat_600SemiBold } from '@expo-google-fonts/montserrat'
 
+import Load from './src/components/Load'
 import { Routes } from './src/routes'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_600SemiBold
+  })
+
+  if (!fontsLoaded) {
+    return <Load />
+  }
+
   return (
     <>
       <StatusBar style="light" hidden={ true }/>
       <Routes/>
     </>
-  );
+  )
 }
