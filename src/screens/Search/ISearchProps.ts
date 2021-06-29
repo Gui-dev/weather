007b2c24441e241b04cddb0b15ae4580295e8c53
Gui-dev <adrianon10@gmail.com>
2021-06-29 02:54:12 -0300

    feat: add interface ISearchProps

---
+++ src/screens/Search/ISearchProps.ts
@@ -0,0 +1,20 @@
+export interface ISearchProps {
+  results: {
+    temp: string
+    city: string
+    city_name: string
+    condition_code: string
+    condition_slug: string
+    currently: string
+    date: string
+    description: string
+    forecast: [{
+      condition: string
+      date: string
+      description: string
+      max: number
+      min: number
+      weekday: string
+    }]
+  }
+}
