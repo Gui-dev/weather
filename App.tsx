4f77d608a6aeeae85f9d0a1667683e1cce74059c
Gui-dev <adrianon10@gmail.com>
2021-06-29 03:30:40 -0300

    feat: add component Load

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { useFonts, Montserrat_400Regular, Montserrat_600SemiBold } from '@expo-google-fonts/montserrat'
 
-import Load from './src/components/Load'
+import { Load } from './src/components/Load'
 import { Routes } from './src/routes'
 
 export default function App() {
