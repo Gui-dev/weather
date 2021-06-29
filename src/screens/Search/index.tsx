b3eb09753412f4c6c62c41bb3d32efd554126c8b
Gui-dev <adrianon10@gmail.com>
2021-06-29 02:53:34 -0300

    feat: add function handleNavigation

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Alert } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { Feather } from '@expo/vector-icons'
 
 import { Background } from './../../components/Background'
-import { Container } from './style'
+import { colors } from '../../styles/colors'
+import { api } from '../../services/api'
+import { ISearchProps } from './ISearchProps'
+import { Container, Back, BackText, SearchBox, SearchInput, SearchButton } from './style'
 
 export const Search: React.FC = () => {
+  const { goBack } = useNavigation()
+  const [inputCity, setInputCity] = useState('')
+  const [city, setCity] = useState<ISearchProps>()
+
+  const handleNavigation = () => {
+    goBack()
+  }
+
+
+
   return (
     <Background>
       <Container>
+        <Back onPress={ handleNavigation }>
+          <Feather name="chevron-left" size={42} color={colors.white}/>
+          <BackText>Voltar</BackText>
+        </Back>
 
+        <SearchBox>
+          <SearchInput
+            placeholder="Ex: São Paulo, SP"
+            value={ inputCity }
+            onChangeText={ setInputCity }
+          />
+          <SearchButton>
+            <Feather name="search" size={32} color={colors.white}/>
+          </SearchButton>
+        </SearchBox>
       </Container>
     </Background>
   )
--- src/screens/Search/style.ts
+++ src/screens/Search/style.ts
@@ -1,5 +1,58 @@
 import styled from 'styled-components/native'
+import { colors } from '../../styles/colors'
+import { fonts } from '../../styles/fonts'
 
-export const Container = styled.View`
-  
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  align-items: center;
+  padding-top: 10%;
+`
+
+export const Back = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.6
+})`
+  align-self: flex-start;
+  flex-direction: row;
+  margin-left: 10px;
+  margin-bottom: 10px;
+`
+
+export const BackText = styled.Text`
+  align-self: center;
+  font-size: 20px;
+  font-family: ${fonts.title};
+  color: ${colors.white};
+`
+
+export const SearchBox = styled.View`
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+  height: 50px;
+  width: 90%;
+  background-color: ${colors.white};
+  border-radius: 8px;
+`
+
+export const SearchInput = styled.TextInput.attrs({
+  placeholderTextColor: colors.dark
+})`
+  font-size: 20px;
+  font-family: ${fonts.normal};
+  color: ${colors.dark};
+  height: 50px;
+  width: 75%;
+  padding-left: 10px;
+`
+
+export const SearchButton = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.6
+})`
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  width: 15%;
+  background-color: ${colors.sun};
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
 `
